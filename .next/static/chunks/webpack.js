3b61337c64558682f1d5051510abe7ecb00559b6
MateusBeltrao <mateusbeltrao2019@gmail.com>
2022-01-26 13:00:17 -0300

    first commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a31c10073e3c55b8"; }
+/******/ 		__webpack_require__.h = function() { return "ed98955dbe9c6477"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
